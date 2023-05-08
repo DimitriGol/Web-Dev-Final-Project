691e1b13c840e85c7994f1664b8e88c9822b3c0c
Tong Chen <tong.chen08@myhunter.cuny.edu>
2023-05-07 21:35:55 -0400

    Finish all basic backend functions

--- Backend/server.js
+++ Backend/server.js
@@ -8,11 +8,19 @@ class Employee extends Model {
 	getDepartment() {
 		return this.department;
 	}
-	getTasks() {
-
+	setFirstName(firstName)
+	{
+		this.firstName = firstName;
+	}
+	setLastName(lastName)
+	{
+		this.lastName = lastName;
+	}
+	setDepartment(department)
+	{
+		this.department = department;
 	}
 }
-
 Employee.init({
 	firstName: {
 		type: DataTypes.STRING,
@@ -41,7 +49,7 @@ class Task extends Model {
 	getUser() {
 		return this.user;
 	}
-	getDiscription() {
+	getDescription() {
 		return this.description;
 	}
 	getPriority() {
@@ -49,6 +57,22 @@ class Task extends Model {
 	}
 	getCompletion() {
 		return this.completion;
+	}
+	setUser(user)
+	{
+		this.user = user;
+	}
+	setDescription(description)
+	{
+		this.description = description;
+	}
+	setPriority(priority)
+	{
+		this.priority = priority;
+	}
+	setCompletion(completion)
+	{
+		this.completion = completion;
 	}
 }
 
@@ -79,21 +103,65 @@ class List extends Model {
 	getEmployees()	{
 		return this.Employees;
 	}
+	getEmployee(x)
+	{
+		return this.Employees[x];
+	}
+	getEmployeeWithTask(x)
+	{
+		return [this.Employees[x],this.getAssociatedTasks(x)];
+	}
 	getTasks() {
 		return this.Tasks;
 	}
-	getAssociatedTasks(employee)
+	getTask(x)
+	{
+		return this.Tasks[x];
+	}
+	getTaskWithEmployee(x)
+	{
+		return [this.Tasks[x],this.Tasks[x].getUser()];
+	}
+	getAssociatedTasks(x)
 	{
 		const Associated_Tasks = [];
 		for(let i = 0; i < this.Tasks.length; i++)
 		{
-			if(this.Tasks[i].getUser() == employee)
+			if(this.Tasks[i].getUser() == this.Employees[x])
 				Associated_Tasks.push(this.Tasks[i]);
 		}
 		return Associated_Tasks;
 	}
+	editEmployee(x, firstName, lastName, department)
+	{
+		this.Employees[x].setFirstName(firstName);
+		this.Employees[x].setLastName(lastName);
+		this.Employees[x].setDepartment(department);
+	}
+	editTask(x, user, description, priority, completion)
+	{
+		this.Tasks[x].setUser(user);
+		this.Tasks[x].setDescription(description);
+		this.Tasks[x].setPriority(priority);
+		this.Tasks[x].setCompletion(completion);
+	}
+	addEmployee(firstName, lastName, department)
+	{
+		this.Employees.push(Employee.build({ firstName: firstName, lastName: lastName, department: department }));
+	}
+	addTask(user, description, priority, completion)
+	{
+		this.Tasks.push(Task.build({ user: user, description: description, priority: priority, completion: completion }));
+	}
+	removeEmployee(x)
+	{
+		this.Employees.splice(x,1);
+	}
+	removeTask(x)
+	{
+		this.Tasks.splice(x,1);
+	}
 }
-
 List.init({
 	Employees: {
 		type: DataTypes.ARRAY(Employee)
@@ -107,35 +175,21 @@ List.init({
 		modelName: 'List'
 	});
 
-const Johnny = Employee.build({ firstName: "Johnny", lastName: "Applesseed", department: "Tech" });
-console.log(Johnny.getFullname());
-console.log(Johnny.getDepartment());
-
-const Lebron = Employee.build({ firstName: "Lebron", lastName: "James", department: "Sports" });
-console.log(Johnny.getFullname());
-console.log(Johnny.getDepartment());
-
-
-const Task1 = Task.build({ user: Johnny, description: "none", priority: 1, completion: 'Not Started' });
-console.log(Task1.getUser());
-console.log(Task1.getDiscription());
-console.log(Task1.getPriority());
-console.log(Task1.getCompletion());
-
-const Task2 = Task.build({ user: Lebron, description: "none", priority: 1, completion: 'Not Started' });
-console.log(Task1.getUser());
-console.log(Task1.getDiscription());
-console.log(Task1.getPriority());
-console.log(Task1.getCompletion());
-
-const Task3 = Task.build({ user: Lebron, description: "none", priority: 1, completion: 'Not Started' });
-console.log(Task1.getUser());
-console.log(Task1.getDiscription());
-console.log(Task1.getPriority());
-console.log(Task1.getCompletion());
-
-const List1 = List.build({Employees: [Johnny, Lebron], Tasks: [Task1,Task2,Task3]});
+const List1 = List.build({Employees: [], Tasks: []});
+List1.addEmployee("Johnny","Applesseed","Tech");
+List1.addEmployee("Lebron","James","Sports");
+List1.addTask(List1.getEmployee(0),"none",1,"Not Started");
+List1.addTask(List1.getEmployee(1),"none",1,"Not Started");
+List1.addTask(List1.getEmployee(1),"none",1,"Not Started");
 console.log(List1.getEmployees());
 console.log(List1.getTasks());
-console.log(List1.getAssociatedTasks(Johnny));
-console.log(List1.getAssociatedTasks(Lebron));
+console.log(List1.getEmployeeWithTask(0));
+console.log(List1.getTaskWithEmployee(1));
+List1.editEmployee(1,"Lebron","James","Tech");
+List1.editTask(1,List1.getEmployee(0),"none",1,"Started");
+console.log(List1.getEmployee(1));
+console.log(List1.getTask(1));
+List1.removeEmployee(0);
+List1.removeTask(0);
+console.log(List1.getEmployees());
+console.log(List1.getTasks());
