cf90e384a51c6156e49dd708381e7bed56b76b80
Dimitri Golyshev <dimitrigolyshev@gmail.com>
2023-05-17 15:06:26 -0400

    Add functions for employee components

--- workspace-frontend/src/Components/Server.js
+++ workspace-frontend/src/Components/Server.js
@@ -25,13 +25,33 @@ const list1 = {
     },
     deleteEmployee: function(x){
         this.employees.splice(x,1)
+        for (let i=0; i<this.tasks.length; i++){
+            if (this.tasks[i].user === x){
+                this.tasks.splice(i,1)
+            }
+        }
     },
+
+    getEmployeeFirstName: function(x){
+        return this.employees[x-1]?.firstName;
+    },
+    getEmployeeLastName: function(x){
+        return this.employees[x-1]?.lastName;
+    },
+
+    getEmployeeDepartment: function(x){
+        return this.employees[x-1]?.department
+    },
+
     addTask: function(user, description, priority, completion) {
         this.tasks.push(new Task(user, description, priority, completion));
     },
     deleteTask: function(x){
         this.tasks.splice(x,1)
     },
+    getTaskUser: function(x){
+        return this.tasks[x-1]?.user;
+    }
 }
 
 export default list1;
