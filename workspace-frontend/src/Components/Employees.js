648892011903b115d6bf63489a92860fe2d86197
Dimitri Golyshev <dimitrigolyshev@gmail.com>
2023-05-17 15:12:04 -0400

    Fix routing, If no employees output message, else output table with all employees

--- workspace-frontend/src/Components/Employees.js
+++ workspace-frontend/src/Components/Employees.js
@@ -15,7 +15,7 @@ function Employees() {
     function deleteEmployee(index) {
         return () => {
             list1.deleteEmployee(index);
-            console.log(list1.employees);
+            // console.log(list1.employees);
             navigate('/employees');
         };
     }
@@ -23,36 +23,40 @@ function Employees() {
     return (
         <div className="App">
             <h1> Employees </h1>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>ID</th>
-                        <th>NAME</th>
-                        <th>DEPARTMENT</th>
-                        <th>ACTIONS</th>
-                    </tr>
-                    {
-                        employees.map((employee, index) =>
-                            <tr key={index + 1}>
-                                <td>{index + 1}</td>
-                                <td>{employee.firstName + " " + employee.lastName}
-                                </td>
-                                <td>{employee.department}</td>
-                                <td>
-                                    <Link to={'/employees/view-employee'}>
-                                        <button className='view-btn'> View </button>
-                                    </Link>
-                                    <Link to={'/edit-employee/' + index}>
-                                        <button className='edit-btn'> Edit </button>
-                                    </Link>
-                                    <button className='delete-btn' onClick={deleteEmployee(index)}> X </button>
-                                </td>
-                            </tr>
-                        )
-                    }
-                </tbody>
-            </table>
-
+            {employees.length === 0 ? (
+                <p className='error-message'> There are no employees. Click the button below to add one. </p>
+            ) : (
+           
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>ID</th>
+                            <th>NAME</th>
+                            <th>DEPARTMENT</th>
+                            <th>ACTIONS</th>
+                        </tr>
+                        {
+                            employees.map((employee, index) =>
+                                <tr key={index + 1}>
+                                    <td>{index + 1}</td>
+                                    <td>{employee.firstName + " " + employee.lastName}
+                                    </td>
+                                    <td>{employee.department}</td>
+                                    <td>
+                                        <Link to={'/employees/' + (index+1)}>
+                                            <button className='view-btn'> View </button>
+                                        </Link>
+                                        <Link to={'/edit-employee/' + (index+1)}>
+                                            <button className='edit-btn'> Edit </button>
+                                        </Link>
+                                        <button className='delete-btn' onClick={deleteEmployee(index)}> X </button>
+                                    </td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            )}
             {/* Add new employee */}
             <div>
                 <Link to='/add-employee'>
