04d7b66067e3baaed398362553fb34499888189a
Dimitri Golyshev <dimitrigolyshev@gmail.com>
2023-05-17 15:08:40 -0400

    Add message if no tasks in database; get rid of hardcoded values, output what is in the database; BUG: doesn't output assigned user

--- workspace-frontend/src/Components/Tasks.js
+++ workspace-frontend/src/Components/Tasks.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import '../App.css';
+
 import list1 from './Server';
 
-function Tasks() {
 
+function Tasks() {
+    // let employees = list1.employees;
     let tasks = list1.tasks;
 
     const navigate = useNavigate();
@@ -13,44 +15,56 @@ function Tasks() {
     function deleteTask(index) {
         return () => {
             list1.deleteTask(index);
-            console.log(list1.tasks);
             navigate('/tasks');
         };
+    }
+
+    function getAssignedUser(id) {
+        let user = list1.getTaskUser(id);
+        return (user);
     }
 
     return (
         <div className="App">
             <h1> Tasks </h1>
-            <table className='task-table'>
-                <tbody>
-                    <tr>
-                        <th>ASSIGNED TO</th>
-                        <th>DESCRIPTION</th>
-                        <th>PRIORITY</th>
-                        <th>COMPLETED</th>
-                        <th>ACTIONS</th
-                    </tr>
-                    {
-                        tasks.map((task, index) =>
-                            <tr key={index + 1}>
-                                <td>{task.user.firstName + " " + task.user.lastName}</td>
-                                <td>{task.description}</td>
-                                <td>{task.priority}</td>
-                                <td>{task.completetion}</td>
-                                <td>
-                                    <Link to={'/tasks/view-task'}>
-                                        <button className='view-btn'> View </button>
-                                    </Link>
-                                    <Link to={'/tasks/edit-task' + index}>
-                                        <button className='edit-btn'> Edit </button>
-                                    </Link>
-                                    <button className='delete-btn' onClick={deleteTask(index)}> X </button>
-                                </td>
-                            </tr>
-                        )
-                    }
-                </tbody>
-            </table>
+            {tasks.length === 0 ? (
+                <p className='error-message'> There are no tasks. Click the button below to add one. </p>
+            ) : (
+                <table className='task-table'>
+                    <tbody>
+                        <tr>
+                            <th>ASSIGNED TO</th>
+                            <th>DESCRIPTION</th>
+                            <th>PRIORITY</th>
+                            <th>COMPLETED</th>
+                            <th>ACTIONS</th>
+                        </tr>
+                        {
+                            tasks.map((task, index) =>
+                                <tr key={index + 1}>
+                                    <td>
+                                        {
+                                            getAssignedUser(index)
+                                        }
+                                    </td>
+                                    <td>{task.description}</td>
+                                    <td>{task.priority}</td>
+                                    <td>{task.completed ? 'Yes' : 'No'}</td>
+                                    <td>
+                                        <Link to={'/tasks/view-task'}>
+                                            <button className='view-btn'> View </button>
+                                        </Link>
+                                        <Link to={'/tasks/edit-task' + index}>
+                                            <button className='edit-btn'> Edit </button>
+                                        </Link>
+                                        <button className='delete-btn' onClick={deleteTask(index)}> X </button>
+                                    </td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            )}
             {/* Add Task Button */}
             <div>
                 <Link to='/add-task'>
