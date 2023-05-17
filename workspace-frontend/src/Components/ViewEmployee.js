611b0fd113e85e73b8f7a0c177ca0fab9474a117
Dimitri Golyshev <dimitrigolyshev@gmail.com>
2023-05-17 15:13:36 -0400

    Get rid of hard-coded values; WHAT TO ADD: message if the employee has no tasks

--- workspace-frontend/src/Components/ViewEmployee.js
+++ workspace-frontend/src/Components/ViewEmployee.js
@@ -2,60 +2,86 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 
+import list1 from './Server';
+let tasks = list1.tasks;
+
+// let lastName = employees[id-1].lastName;
+
 function ViewEmployee() {
+    
+    let id = window.location.href.split('employees/').pop();
+    
+    function getEmployeeFullName(id) {
+        let firstName = list1.getEmployeeFirstName(id);
+        let lastName = list1.getEmployeeLastName(id);
+        return (firstName + " " + lastName);
+    }
+
+    function getEmployeeDepartment(id){
+        let department = list1.getEmployeeDepartment(id);
+        return (department);
+    }
+    
+    // //Checks to see if the employee has tasks
+    // function employeeHasTasks(id){
+    //    //Implement here   
+    // }
+    
+
     return (
         <div className="App">
-            <h1 className='single-employee-title'> Single Employee View</h1>
-            <p className='single-employee-department'> Department: goes here{/* department goes here */}</p>
-            <div>
-                <table>
 
+            <h1 className='single-employee-title'> Name: {getEmployeeFullName(id)}</h1>
+            <p className='single-employee-department'> Department: {getEmployeeDepartment(id)}</p>
+            {/* {employeeHasTasks(id) ? (
+                <div>
+                    <p className='error-message'> There are no tasks assigned to this employee. Click the button below to add one. </p>
+                    <div>
+                        <Link to='/add-task'>
+                            <button className='add-btn'> Add Task </button>
+                        </Link>
+                    </div>
+                </div>
+            ) : ( */}
+                <div>
+                    <table>
+                        <tbody>
 
+                            <tr>
+                                <th>DESCRIPTION</th>
+                                <th>PRIORITY</th>
+                                <th>COMPLETED</th>
+                                <th>ACTIONS</th>
+                            </tr>
+                            
+                            {
+                                tasks.map((task, index) => {
+                                    // if (task.user === ()) {
+                                        return (
+                                            <tr key={index}>
+                                                <td>{task.description}</td>
+                                                <td>{task.priority}</td>
+                                                <td>{task.completion}</td>
+                                                <td>
+                                                    <Link to={`/tasks/${index}`}>
+                                                        <button className='view-btn'> View </button>
+                                                    </Link>
+                                                </td>
+                                            </tr>
+                                        )
+                                    // }
+                                })
+                            }
 
-                    <tbody>
+                        </tbody>
 
-                        <tr>
-                            <th>DESCRIPTION</th>
-                            <th>PRIORITY</th>
-                            <th>COMPLETED</th>
-                            <th>ACTIONS</th>
-                        </tr>
-                        
-                        <tr>
-                            <td>Sample</td>
-                            <td>Medium</td>
-                            <td>No</td>
-                            <td>
-                                <button className='view-btn'> View </button>
-                            </td>
-                        </tr>
+                    </table>
 
-                        <tr>
-                            <td>Make frontend responsive</td>
-                            <td>Low</td>
-                            <td>No</td>
-                            <td>
-                                <button className='view-btn'> View </button>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>Debug code in server.js file</td>
-                            <td>High</td>
-                            <td>Yes</td>
-                            <td>
-                                <button className='view-btn'> View </button>
-                            </td>
-                        </tr>
-
-                    </tbody>
-
-                </table>
-
-                <Link to='/employees'>
-                    <button className='back-btn'> Back </button>
-                </Link>
-            </div>
+                    <Link to='/employees'>
+                        <button className='back-btn'> Back </button>
+                    </Link>
+                </div>
+            {/* )} */}
         </div>
     )
 } 
